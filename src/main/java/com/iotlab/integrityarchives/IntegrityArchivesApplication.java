efeb0d8f9a10129504c2e2175219cea0153a203d
Nixxxxx <jianghao091@gmail.com>
2019-04-13 15:44:38 +0800

    init project

---
+++ src/main/java/com/iotlab/integrityarchives/IntegrityArchivesApplication.java
@@ -0,0 +1,13 @@
+package com.iotlab.integrityarchives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrityArchivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrityArchivesApplication.class, args);
+	}
+
+}
